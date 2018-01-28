ab08f383d7367b6430a9ef5be56a274f7d8eb2dd
Maria <mariajescobar10@gmail.com>
2018-01-27 18:44:30 -0600

    clean up commit

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,15 +1,48 @@
 class GamesController < ApplicationController
   def name_action
     input_name = params[:name]
-    name_upcase = input_name.upcase
+    name_upcased = input_name.upcase
 
-    if name_upcase.first == "A"
+    if name_upcased.first == "A"
       output_message = "Hey your name starts with an A."
-    end 
+    end
 
-    render json: {your_name: name_upcase, 
+    render json: {
+                  your_name: name_upcased,
                   message: output_message
-                 }
+                  }
+  end
 
-  end 
+  def guess_query_action
+    winning_number = 42
+
+    input_guess = params[:user_guess].to_i
+
+
+    if input_guess > winning_number
+      output_message = "Try Lower"
+    elsif input_guess < winning_number
+      output_message = "Go Higher"
+    else
+      output_message = "You Win!!!!"
+    end
+
+    render json: {guess: input_guess, message: output_message}
+  end
+
+  def guess_segment_action
+    winning_number = 42
+
+    input_guess = params[:segment_guess].to_i
+
+    if input_guess > winning_number
+      output_message = "Try Lower"
+    elsif input_guess < winning_number
+      output_message = "Go Higher"
+    else
+      output_message = "You Win!!!!"
+    end
+
+    render json: {guess: input_guess, message: output_message}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get '/name_url' => 'games#name_action'
+  get '/guess_query' => 'games#guess_query_action'
+  get '/guess_segment/:segment_guess' => 'games#guess_segment_action'
 end
